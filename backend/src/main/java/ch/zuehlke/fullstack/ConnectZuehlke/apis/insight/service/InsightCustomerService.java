92337801979ee01b1906ff57d3de71bd99dd2834
Tobias Forster <tobias.forster@zuehlke.com>
2019-03-20 16:35:42 +0100

    Remove the customer integration from the backend.

--- backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/apis/insight/service/InsightCustomerService.java
+++
@@ -1,9 +0,0 @@
-package ch.zuehlke.fullstack.ConnectZuehlke.apis.insight.service;
-
-import ch.zuehlke.fullstack.ConnectZuehlke.domain.Customer;
-
-import java.util.List;
-
-public interface InsightCustomerService {
-    List<Customer> getCustomers();
-}
