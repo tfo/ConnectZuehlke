13055a8dca3087ecbb2d2dbf8df8cd41c36ea808
Tobias Forster <tobias.forster@zuehlke.com>
2019-03-21 20:09:07 +0100

    Create a Jasmine test for the GameStateService.

---
+++ frontend/src/app/service/game-state/game-state-test-data.ts
@@ -0,0 +1,80 @@
+import {Employee} from "../../domain/Employee";
+import {Answer} from "../../domain/Answer";
+import {Question} from "../../domain/Question";
+import {Game} from "../../domain/Game";
+
+export class GameStateTestData {
+
+  static employee01() : Employee {
+    return new Employee('John', 'Doe', 1, 'jdo', 'CH');
+  }
+
+  static employee02() : Employee {
+    return new Employee('Max', 'Mustermann', 2, 'mmu', 'CH');
+  }
+
+  static employee03() : Employee {
+    return new Employee('Rita', 'Richterich', 3, 'riri', 'DE');
+  }
+
+  static employee04(): Employee {
+    return new Employee('Janine', 'Wilson', 4, 'wija', 'DE');
+  }
+
+  static selectedEmployee(): Employee {
+    return this.employee02();
+  }
+
+  static question01(): Question {
+    return new Question('q1', 'What gender is the person?', [
+      this.question01_answer01(),
+      this.question01_answer02()
+    ]);
+  }
+
+  static question01_answer01(): Answer {
+    return new Answer('q1-a1', 'Male', [1, 2], 'The person is male.');
+  }
+
+  static question01_answer02(): Answer {
+    return new Answer('q1-a2', 'Female', [3, 4], 'The person is female.');
+  }
+
+  static question02(): Question {
+    return new Question('q2', 'Where does the person work?', [
+      this.question02_answer01(),
+      this.question02_answer02()
+    ]);
+  }
+
+  static question02_answer01(): Answer {
+    return new Answer('q2-a1', 'Switzerland', [1], 'The person works in Switzerland.');
+  }
+
+  static question02_answer02(): Answer {
+    return new Answer('q2-a2', 'Everywhere else', [2, 3, 4], 'The person works somewhere else.');
+  }
+
+  static question03(): Question {
+    return new Question('q3', 'What\'s the role of the person?', [
+      this.question03_answer01(),
+      this.question03_answer02()
+    ]);
+  }
+
+  static question03_answer01(): Answer {
+    return new Answer('q3-a1', 'Engineer', [2, 3], 'The person is engineer.');
+  }
+
+  static question03_answer02(): Answer {
+    return new Answer('q3-a1', 'Everything else', [1, 4], 'The person is something else.');
+  }
+
+  static game(): Game {
+    return new Game(
+      'game-id',
+      [this.employee01(), this.employee02(), this.employee03(), this.employee04()],
+      this.selectedEmployee(),
+      [this.question01(), this.question02(), this.question03()]);
+  }
+}
---
+++ frontend/src/app/service/game-state/game-state.service.spec.ts
@@ -0,0 +1,82 @@
+import {async, TestBed} from '@angular/core/testing';
+import {GameStateService} from './game-state.service';
+import {GameStateTestData} from "./game-state-test-data";
+import {Game} from "../../domain/Game";
+
+describe('GameStateService', () => {
+  let service: GameStateService;
+  let game: Game;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [],
+      imports: [],
+      providers: [
+        GameStateService
+      ]
+    });
+
+    game = GameStateTestData.game();
+    service = TestBed.get(GameStateService);
+    service.initialize(game);
+  }));
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should disable employees with wrong answer match', () => {
+    const question03 = game.questions[2];
+    const actual = service.guess(question03, question03.answers[1]);
+
+    expect(actual).toEqual(false);
+
+    const hiddenEmployees = service.employees.filter(employee => employee.hidden);
+    const visibleEmployees = service.employees.filter(employee => !employee.hidden);
+
+    expect(hiddenEmployees).toContain(game.employees[0]);
+    expect(hiddenEmployees).toContain(game.employees[3]);
+    expect(visibleEmployees).toContain(game.employees[1]);
+    expect(visibleEmployees).toContain(game.employees[2]);
+    expect(visibleEmployees).toContain(game.selectedEmployee);
+  });
+
+  it('should disable employees having not right answer match', () => {
+    const question01 = game.questions[0];
+    const actual = service.guess(question01, question01.answers[0]);
+
+    expect(actual).toEqual(true);
+
+    const hiddenEmployees = service.employees.filter(employee => employee.hidden);
+    const visibleEmployees = service.employees.filter(employee => !employee.hidden);
+
+    expect(hiddenEmployees).toContain(game.employees[2]);
+    expect(hiddenEmployees).toContain(game.employees[3]);
+    expect(visibleEmployees).toContain(game.employees[0]);
+    expect(visibleEmployees).toContain(game.employees[1]);
+    expect(visibleEmployees).toContain(game.selectedEmployee);
+  });
+
+  it('should disable all employees when won', () => {
+    const question01 = game.questions[0];
+    const actual1 = service.guess(question01, question01.answers[1]);
+
+    expect(actual1).toEqual(false);
+    expect(service.hasWon).toEqual(false);
+
+    const question02 = game.questions[1];
+    const actual2 = service.guess(question02, question02.answers[1]);
+
+    expect(actual2).toEqual(true);
+    expect(service.hasWon).toEqual(true);
+
+    const hiddenEmployees = service.employees.filter(employee => employee.hidden);
+    const visibleEmployees = service.employees.filter(employee => !employee.hidden);
+
+    expect(hiddenEmployees).toContain(game.employees[0]);
+    expect(hiddenEmployees).toContain(game.employees[2]);
+    expect(hiddenEmployees).toContain(game.employees[3]);
+    expect(visibleEmployees).toContain(game.employees[1]);
+    expect(visibleEmployees).toContain(game.selectedEmployee);
+  });
+});
