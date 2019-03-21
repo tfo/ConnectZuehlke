94283542ced5ba88c605f2930ad483ea3c30ef38
Andres <andres.konrad@hotmail.com>
2019-03-21 08:31:39 +0100

    Added Answer and Question Classes

--- backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/domain/Game.java
+++ backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/domain/Game.java
@@ -1,5 +1,6 @@
 package ch.zuehlke.fullstack.ConnectZuehlke.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Game {
@@ -7,11 +8,18 @@ public class Game {
     private final String id;
     private final List<Employee> employees;
     private final Employee selectedEmployee;
+    private final List<Question> questions;
 
     public Game(String id, List<Employee> employees, Employee selectedEmployee) {
         this.id = id;
         this.employees = employees;
         this.selectedEmployee = selectedEmployee;
+        this.questions = createQuestions();
+    }
+
+    private List<Question> createQuestions() {
+        //TODO create questions based on employees and selectedEmployee
+        return Collections.emptyList();
     }
 
     public Employee getSelectedEmployee() {
