6785817157d7777476fa3c5bf03d4c32b3bd8e9c
Tobias Forster <tobias.forster@zuehlke.com>
2019-03-21 08:38:41 +0100

    Introduce the GameRestService as REST stub in the frontend application.

---
+++ frontend/src/app/service/game.service-mock.ts
@@ -0,0 +1,16 @@
+import {Game} from '../domain/Game';
+import {EMPLOYEES} from '../employee.service-mock';
+import {GameService} from './game.service';
+import {Observable, of} from 'rxjs';
+
+export const GAME: Game = {
+  id: 'game-id',
+  employees: EMPLOYEES,
+  selectedEmployee: EMPLOYEES[2]
+};
+
+export class GameServiceMock implements GameService {
+  createNewGame(): Observable<Game> {
+    return of(GAME);
+  }
+}
