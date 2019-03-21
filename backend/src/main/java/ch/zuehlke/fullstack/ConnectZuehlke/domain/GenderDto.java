992007463e503fc49d33d49c59a90ea99910917d
Andres <andres.konrad@hotmail.com>
2019-03-21 19:57:49 +0100

    add gender problem solution fix bug in frontend

---
+++ backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/domain/GenderDto.java
@@ -0,0 +1,51 @@
+package ch.zuehlke.fullstack.ConnectZuehlke.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Objects;
+
+public class GenderDto {
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("gender")
+    private String gender;
+    @JsonProperty("probability")
+    private double probability;
+    @JsonProperty("count")
+    private int count;
+
+    //for json deserialization
+    public GenderDto() {
+    }
+
+    public GenderDto(String name, String gender, double probability, int count) {
+        this.name = name;
+        this.gender = gender;
+        this.probability = probability;
+        this.count = count;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public double getProbability() {
+        return probability;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GenderDto gender1 = (GenderDto) o;
+        return Double.compare(gender1.probability, probability) == 0 &&
+                count == gender1.count &&
+                Objects.equals(name, gender1.name) &&
+                Objects.equals(gender, gender1.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, gender, probability, count);
+    }
+}
--- backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/service/GameService.java
+++ backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/service/GameService.java
@@ -6,8 +6,12 @@ import ch.zuehlke.fullstack.ConnectZuehlke.domain.Game;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
@@ -15,14 +19,130 @@ import java.util.UUID;
 @Service
 public class GameService {
 
+    private final RestTemplate genderizeRestTemplate;
+    @Value("${genderize.url}")
+    private String genderizeUrl;
     private static final Logger LOGGER = LoggerFactory.getLogger(GameService.class);
 
     private final InsightEmployeeService employeeService;
 
     @Autowired
-    public GameService(InsightEmployeeService employeeService) {
+    public GameService(InsightEmployeeService employeeService,
+                       RestTemplateBuilder restTemplateBuilder) {
+        genderizeRestTemplate = restTemplateBuilder
+                .rootUri(genderizeUrl)
+                .build();
         this.employeeService = employeeService;
     }
+
+    private static final List<String> swappedMaleToFemale = Arrays.asList(
+            "sigl",
+            "caas",
+            "mase",
+            "fras",
+            "lonk",
+            "osan",
+            "icke",
+            "allo",
+            "amsa",
+            "mavi",
+            "juwe",
+            "yuwa",
+            "vifi",
+            "elvu",
+            "tapl",
+            "stom",
+            "nate",
+            "vikr",
+            "vemi",
+            "sodo",
+            "krst",
+            "kean",
+            "tkrs",
+            "anmi",
+            "susu",
+            "ruvi",
+            "joeh",
+            "kara",
+            "leba",
+            "make",
+            "anre",
+            "acif",
+            "kadi",
+            "kaal",
+            "vini",
+            "cehe",
+            "mapo",
+            "kool",
+            "emgr",
+            "mjan",
+            "stan",
+            "albr",
+            "chka",
+            "tang",
+            "insc",
+            "likl",
+            "isro",
+            "doja",
+            "ero",
+            "jatr",
+            "sfo",
+            "marb",
+            "anha",
+            "toma",
+            "issc",
+            "sasc",
+            "coeg",
+            "anen",
+            "aku",
+            "ami",
+            "fme",
+            "codo",
+            "sge",
+            "doro",
+            "piwi",
+            "chh",
+            "clsa",
+            "hopa",
+            "bekn",
+            "voma",
+            "alli",
+            "mele",
+            "lios",
+            "rist",
+            "pai",
+            "jse",
+            "dahu",
+            "rapo",
+            "irca",
+            "clar",
+            "tiko",
+            "vigo",
+            "mera",
+            "maco",
+            "kaba",
+            "kwl",
+            "kage",
+            "anra",
+            "held",
+            "brad",
+            "kafa",
+            "anst",
+            "duna",
+            "ansh",
+            "basc",
+            "stil",
+            "dats",
+            "yuca",
+            "asla",
+            "alim",
+            "suen",
+            "inli",
+            "jeli",
+            "vga",
+            "lich",
+            "jpro"
+    );
 
     public Game createGame(int numberOfEmployees) {
         if (numberOfEmployees <= 4) {
@@ -30,6 +150,8 @@ public class GameService {
         }
 
         List<Employee> allEmployees = this.employeeService.getEmployees();
+        overrideGender(allEmployees);
+
         Game game = null;
         boolean foundUniqueSolution = false;
 
@@ -46,6 +168,33 @@ public class GameService {
 
         return game;
     }
+
+    private void overrideGender(List<Employee> allEmployees) {
+        for (Employee employee : allEmployees) {
+            if (swappedMaleToFemale.contains(employee.getCode())) {
+                employee.setGender(1);
+            }
+        }
+    }
+
+    /*
+    private void overrideGender(List<Employee> allEmployees) {
+        for (Employee employee : allEmployees) {
+            if (employee.getGender() > 0) continue; //skip if it's not default male
+            GenderDto gender = genderizeRestTemplate.getForObject(genderizeUrl+ "?name=" + employee.getFirstName(), GenderDto.class);
+            if (gender != null && gender.getGender() != null) adaptGenderBasedOnGenderizeApi(employee, gender);
+        }
+    }
+
+    private static void adaptGenderBasedOnGenderizeApi(Employee employee, GenderDto gender) {
+        int genderInt = gender.getGender().equalsIgnoreCase("female") ? 1 : 0;
+        if (genderInt != employee.getGender()) {
+            LOGGER.info("Mismatch!: " + employee.getCode() + " " + employee.getFirstName() + " " + employee.getLastName());
+            LOGGER.info("GenderDto: " + gender.getGender());
+            LOGGER.info("Probability: " + gender.getProbability());
+        }
+    }
+    */
 
     private List<Employee> chooseEmployees(List<Employee> allEmployees, int numberOfEmployees) {
         Collections.shuffle(allEmployees);
