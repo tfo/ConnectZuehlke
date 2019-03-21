c2e6dc07eb2d4ef72d91678044d2e810e7ec525e
Leotrim Zulfiu (taazule2) <leotrim.zulfiu@swisscom.com>
2019-03-21 02:33:29 -0700

    re-order mock employees

--- frontend/src/app/service/employee/employee.service-mock.ts
+++ frontend/src/app/service/employee/employee.service-mock.ts
@@ -1,9 +1,9 @@
 import {Employee} from '../../domain/Employee';
 
 export const SECRET_EMPLOYEES: Employee[] = [
-  {firstName: 'John', lastName: 'Doe', id: 1, code: 'jdo', pictureId: '2'},
   {firstName: 'Max', lastName: 'Mustermann', id: 2, code: 'mmu', pictureId: '7'},
   {firstName: 'Rita', lastName: 'Richterich', id: 3, code: 'riri', pictureId: '32'},
+  {firstName: 'John', lastName: 'Doe', id: 1, code: 'jdo', pictureId: '2'},
   {firstName: 'Janine', lastName: 'Wilson', id: 4, code: 'wija', pictureId: '43'},
   {firstName: 'Santiago', lastName: 'Garcias', id: 5, code: 'saga', pictureId: '54'},
   {firstName: 'Donald', lastName: 'Mratsov', id: 6, code: 'domr', pictureId: '66'},
