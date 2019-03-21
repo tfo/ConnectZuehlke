d2ce00dd7ef564ce4a9323f8c336ee4915cf1bb9
Tobias Forster <tobias.forster@zuehlke.com>
2019-03-21 13:11:00 +0100

    Integrate the game state properly in the game component.

--- frontend/src/app/game/game.component.ts
+++ frontend/src/app/game/game.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {GameServiceMock} from '../service/game/game.service-mock';
 import {GameStateService} from '../service/game-state/game-state.service';
-import {Question} from '../domain/Question';
-import {Employee} from '../domain/Employee';
 
 @Component({
   selector: 'app-game',
@@ -10,8 +8,6 @@ import {Employee} from '../domain/Employee';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
-  secretEmployees: Employee[];
-  questions: Question[];
 
   constructor(private gameService: GameServiceMock, private gameState: GameStateService) {
   }
@@ -19,12 +15,6 @@ export class GameComponent implements OnInit {
   ngOnInit() {
     this.gameService.createNewGame().subscribe(game => {
       this.gameState.initialize(game);
-      this.secretEmployees = game.employees;
-      this.questions = game.questions;
     });
-  }
-
-  isGrayedOut() {
-    return Math.random() > 0.5;
   }
 }
