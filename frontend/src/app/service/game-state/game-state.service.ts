39377c4ee1d73b7d29f64fc4cb812f5a9fb589af
Tobias Forster <tobias.forster@zuehlke.com>
2019-03-21 11:34:03 +0100

    Extend the GameStateService with a guess method.

--- frontend/src/app/service/game-state/game-state.service.ts
+++ frontend/src/app/service/game-state/game-state.service.ts
@@ -2,6 +2,7 @@ import {Game} from "../../domain/Game";
 import {Employee} from "../../domain/Employee";
 import {Question} from "../../domain/Question";
 import {Injectable} from "@angular/core";
+import {Answer} from "../../domain/Answer";
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,9 @@ export class GameStateService {
   secretEmployee: Employee;
   questions: Question[];
 
+  questionCount: number;
+  missedQuestionCount: number;
+
   constructor() {}
 
   public initialize(game: Game): void {
@@ -20,6 +24,23 @@ export class GameStateService {
     this.employees = game.employees;
     this.secretEmployee = game.selectedEmployee;
     this.questions = game.questions;
+
+    this.questionCount = 0;
+    this.missedQuestionCount = 0;
   }
 
+  public guess(answer: Answer): boolean {
+    this.questionCount++;
+
+    let matchSecretEmployee = this.matchSecretEmployee(answer);
+    if (matchSecretEmployee === true) {
+      this.missedQuestionCount++;
+    }
+
+    return matchSecretEmployee;
+  }
+
+  private matchSecretEmployee(answer: Answer): boolean {
+    return answer.ids.includes(this.secretEmployee.id);
+  }
 }
