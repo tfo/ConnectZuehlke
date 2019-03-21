5960fa88398f607fb0fde6957ece7d6c2f15413e
Tobias Forster <tobias.forster@zuehlke.com>
2019-03-21 09:24:37 +0100

    Code cleanup.

--- frontend/src/app/service/game/game.service-mock.ts
+++ frontend/src/app/service/game/game.service-mock.ts
@@ -1,13 +1,13 @@
 import {GameService} from './game.service';
 import {Observable, of} from 'rxjs';
-import {Game} from "../../domain/Game";
-import {EMPLOYEES} from "../employee/employee.service-mock";
-import {Question} from "../../domain/Question";
+import {Game} from '../../domain/Game';
+import {EMPLOYEES} from '../employee/employee.service-mock';
+import {Question} from '../../domain/Question';
 
 export const QUESTIONS: Question[] = [
-  {title: "What gender is the person?", answers: [{title: "Male", ids: [1,2]}, {title: "Female", ids: [3,4]}]},
-  {title: "Where does the person work?", answers: [{title: "Switzerland", ids: [1]}, {title: "Everywhere else", ids: [2,3,4]}]},
-  {title: "What's the role of the person?", answers: [{title: "Engineer", ids: [2,3]}, {title: "Everything else", ids: [1,4]}]}
+  {title: 'What gender is the person?', answers: [{title: 'Male', ids: [1,2]}, {title: 'Female', ids: [3,4]}]},
+  {title: 'Where does the person work?', answers: [{title: 'Switzerland', ids: [1]}, {title: 'Everywhere else', ids: [2,3,4]}]},
+  {title: 'What\'s the role of the person?', answers: [{title: 'Engineer', ids: [2,3]}, {title: 'Everything else', ids: [1,4]}]}
   ];
 
 export const GAME: Game = {
